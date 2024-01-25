591659cdf3cc384eaee1dc031af9e0ac4705b801
indhuchandrasekar <156568683+indhuchandrasekar@users.noreply.github.com>
2024-01-25 15:52:37 +0530

    Add files via upload

---
+++ Task-4 Prb-1.py
@@ -0,0 +1,17 @@
+Create a program class called circle with constructor which will take a list as an argument for the task.
+#The List is[10,501,22,37,100,999,87,351]
+class Circle:
+# constructor
+def __init__(self):
+# initializing instance variable
+self.num=[10,501,22,37,100,999,87,351]
+
+# a method
+def read_number(self):
+print(self.num)
+
+# creating object of the class. This invokes constructor
+obj = Circle()
+
+# calling the instance method using the object obj
+obj.read_number()
